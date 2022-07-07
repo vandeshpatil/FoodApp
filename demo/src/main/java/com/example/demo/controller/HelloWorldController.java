024f388401a50e27936e939585f0dc9fc54490f3
vandeshpatil <vandeshpatil2@gmail.com>
2022-07-07 20:37:36 +0530

    hi

---
+++ demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin()
+public class HelloWorldController {
+
+	@RequestMapping({ "/hello" })
+	@PreAuthorize("hasRole('ADMIN')")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
